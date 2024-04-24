04b33b02251aff7dc8728bf973570e624e9fa9e2
David Sabzanov <dsabzanov@gmail.com>
2024-04-24 00:13:29 -0700

    Initial upload of GesturCalcV2 project to GitHub

---
+++ SwiftUI/GesturCalcV2/GesturCalcV2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GesturCalcV2
+//
+//  Created by David Sabzanov on 4/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/GesturCalcV2/GesturCalcV2/GesturCalcV2App.swift
@@ -0,0 +1,17 @@
+//
+//  GesturCalcV2App.swift
+//  GesturCalcV2
+//
+//  Created by David Sabzanov on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct GesturCalcV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
